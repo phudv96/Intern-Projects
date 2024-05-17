8ddc622267ccd66145cdeb504ec7e6ee0861fe2f
Hiep Phung Hoang <intern.phhiep@tma.com.vn>
2024-05-17 16:23:52 +0700

    cleaning up code, indentation, semicolon

--- Node_Express/route/task.js
+++ Node_Express/route/task.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const {getAllTasks, 
     createTask, 
     getTask, 
     updateTask, 
     deleteTask} = require('../controller/task'); //importing functions from controller
 
-router.route('/').get(getAllTasks).post(createTask) //specifying what to do when receiving GET and POST request at /
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)//same goes for /:id
+router.route('/').get(getAllTasks).post(createTask); //specifying what to do when receiving GET and POST request at /
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);//same goes for /:id
 
 module.exports = router
